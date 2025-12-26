8911ac00412e0d143e8b70b53064ccf59402b71a
RitikaIGDTUW <ritikash06@gmail.com>
2025-12-26 22:54:30 +0530

    implement hoc

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Navbar from '#components/Navbar';
 import Welcome from '#components/Welcome';
 import Dock from "#components/Dock"
+import WindowControls from '#components/WindowControls';
 import React from 'react'
 
-export {Navbar, Welcome, Dock};
+export {Navbar, Welcome, Dock, WindowControls};
